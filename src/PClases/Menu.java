02620f88a5ab53c08ae5944779c6514662cb2315
vKxno <cristian36722@gmail.com>
2023-05-10 22:32:46 -0600

    Version 1 del proyecto final (contiene main)

---
+++ src/PClases/Menu.java
@@ -0,0 +1,24 @@
+//@author crist
+package PClases;
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        
+        System.out.println("Ingrese el numero para escoger el programa: ");
+        opcion = entrada.nextInt();
+        
+        switch(opcion){
+            case 1 ->{
+            }
+            case 2 ->{
+                
+            }
+            default ->{
+                
+            }
+        }
+    }
+}
