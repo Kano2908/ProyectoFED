68c526f46447d475115d9e0a92bbbb7bab3cf445
vKxno <cristian36722@gmail.com>
2023-05-11 00:07:06 -0600

    Version 1.1 del proyecto final (contiene main)

--- src/PClases/Menu.java
+++ src/PClases/Menu.java
@@ -1,57 +1,108 @@
 //@author crist
 package PClases;
+
 import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int opcion;
-        
+
         do {
-            System.out.println("Ingrese el numero para escoger el programa: ");
+            System.out.println("------ MENU ------");
+            System.out.println("Lista De Programas: Estrucura De Datos");
+            System.out.println("Ingrese El Numero Para Escoger El Programa: ");
             opcion = entrada.nextInt();
 
             switch (opcion) {
                 case 1 -> {
+                    do {
+
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 2 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 3 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 4 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 5 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 6 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 7 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 8 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 9 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 10 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 case 11 -> {
+                    do {
 
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
-                case 20 -> {
-                    System.out.println("Adios");
+                case 12 -> {
+                    do {
+
+                        System.out.println("Quieres Salir = 1 / Repetir Programa = 0");
+                        opcion = entrada.nextInt();
+                    } while (opcion == 0);
                 }
                 default -> {
-
+                    System.out.println("Error Seleccione Un Numero Valido");
                 }
             }
-        } while (opcion != 20);
-        
+        } while (opcion != 13);
     }
 }
