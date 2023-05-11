467335822b585d5532b7201cc518921af983ac52
vKxno <cristian36722@gmail.com>
2023-05-10 23:54:46 -0600

    Version 1.1 del proyecto final (contiene main)

--- src/PClases/Menu.java
+++ src/PClases/Menu.java
@@ -7,21 +7,51 @@ public class Menu {
         Scanner entrada = new Scanner(System.in);
         int opcion;
         
-        System.out.println("Ingrese el numero para escoger el programa: ");
-        opcion = entrada.nextInt();
+        do {
+            System.out.println("Ingrese el numero para escoger el programa: ");
+            opcion = entrada.nextInt();
+
+            switch (opcion) {
+                case 1 -> {
+                }
+                case 2 -> {
+
+                }
+                case 3 -> {
+
+                }
+                case 4 -> {
+
+                }
+                case 5 -> {
+
+                }
+                case 6 -> {
+
+                }
+                case 7 -> {
+
+                }
+                case 8 -> {
+
+                }
+                case 9 -> {
+
+                }
+                case 10 -> {
+
+                }
+                case 11 -> {
+
+                }
+                case 20 -> {
+                    System.out.println("Adios");
+                }
+                default -> {
+
+                }
+            }
+        } while (opcion != 20);
         
-        switch(opcion){
-            case 1 ->{
-            }
-            case 2 ->{
-                
-            }
-            case 3->{
-                
-            }
-            default ->{
-                
-            }
-        }
     }
 }
